635b5bc7b0f32f32c1cc780fa92ebc78aa812892
Alexander Kolev <165174435+aleksmkolev@users.noreply.github.com>
2024-05-15 22:00:35 +0100

    Array sorting by 2 criteria +

---
+++ sortArr2Crit.js
@@ -0,0 +1,11 @@
+function solve(input) {
+    const twoCriteriaSort = (cur, next) =>
+        cur.length - next.length || cur.localeCompare(next);
+    input.sort(twoCriteriaSort);
+    console.log(input.join('\n'));
+}
+solve(['alpha',
+
+'beta',
+
+'gamma'])
